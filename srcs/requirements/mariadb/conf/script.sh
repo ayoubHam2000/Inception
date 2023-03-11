ef3fd93faadc580c75b04e9da9dda93d35c1b090
aben-ham <ayoubbenhamou0731@gmail.com>
2023-03-11 10:41:48 +0100

    set up nginx mariadb and wordpress with php-fpm

---
+++ srcs/requirements/mariadb/conf/script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+killall -15 mysqld
+killall -15 mysqld_safe
+
+mysqld_safe --skip-grant-tables --skip-networking & (\
+    sleep 3;
+    
+    mysql -u root -e "FLUSH PRIVILEGES;"
+
+    mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB_NAME;"
+    
+    mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+    GRANT ALL PRIVILEGES ON $MYSQL_DB_NAME.* TO '$MYSQL_USER'@'%'"
+    
+    mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_ADMIN'@'%' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD';\
+    GRANT ALL PRIVILEGES ON $MYSQL_DB_NAME.* TO '$MYSQL_ADMIN'@'%'"
+
+    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+    
+    killall -15 mysqld;
+    killall -15 mysqld_safe;
+    ) &> /dev/null;
+
+echo "Starting mariadb ..."
+wait
+echo "Done."
+mysqld_safe
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,1 +1,20 @@
-service php7.4-fpm start
+#!/bin/bash
+
+cd /var/www/aben-ham.42.fr/html
+pwd
+
+if ! test -f "wp-config.php"
+then
+while true
+do
+	if sudo -u aben-ham.42.fr wp config create --dbname="$MYSQL_DB_NAME" --dbuser="$MYSQL_ADMIN" --dbpass="$MYSQL_ADMIN_PASSWORD" --dbhost="mariadb" &> /dev/null; then
+		break
+	else
+		echo "Waiting Data Base To Start ...";
+		sleep 1;
+	fi
+done
+sudo -u aben-ham.42.fr wp core install --url="$WORDPRESS_URL" --title="$WORDPRESS_TITLE" --admin_user="$WORDPRESS_ADMIN" --admin_password="$WORDPRESS_PASS" --admin_email="$WORDPRESS_ADMIN_EMAIL"
+fi
+
+php-fpm7.3 -F -R 
